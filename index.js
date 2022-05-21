2785796c1fc24e559f5c561849a05a303c0e4f55
rezainiet <masudrezaog@gmail.com>
2022-05-22 04:29:48 +0600

    created available api and booking api

--- index.js
+++ index.js
@@ -30,11 +30,48 @@ async function run() {
             res.send(result);
         });
 
+        app.get('/booking', async (req, res) => {
+            const patient = req.query.patient;
+            const query = { patient: patient };
+            const bookings = await bookingCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/booking', async (req, res) => {
             const booking = req.body;
+            const query = { treatment: booking.treatment, date: booking.date, patient: booking.patient };
+            const exists = await bookingCollection.findOne(query);
+            if (exists) {
+                return res.send({ success: false, booking: exists })
+            }
             const result = await bookingCollection.insertOne(booking);
-            res.send(result);
-        })
+            res.send({ success: true, result });
+        });
+
+        app.get('/available', async (req, res) => {
+            const date = req.query.date;
+
+            // step 1: get all services
+            const services = await servicesCollection.find().toArray();
+
+            //  step 2: get the bookings of that day. output: [{}, {}, {}, {}, {}]
+            const query = { date: date };
+            const bookings = await bookingCollection.find(query).toArray();
+
+            // step 3: for each service
+            services.forEach(service => {
+                // step 4: find bookings for that service. output [{}, {}, {}, {}]
+                const serviceBookings = bookings.filter(book => book.treatment === service.name);
+                // step 5: select slots for the service bookings: ['', '', '', '']
+                const bookedSlots = serviceBookings.map(book => book.slot);
+                //  step 6: select those slots that are not in bookedslots
+                const available = service?.slots?.filter(slot => !bookedSlots.includes(slot));
+                //  step 7: set available to slots to make easier
+                service.slots = available;
+            })
+
+            res.send(services);
+        });
     }
     finally {
 
